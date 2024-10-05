33492c78489774601f7b9288a30139596ff05e88
pixelchai <ppozogo@gmail.com>
2024-10-05 16:21:11 +0100

    potentially work on patching entries

---
+++ src/utils/patchEntry.ts
@@ -0,0 +1,11 @@
+
+// import type { CollectionEntry } from "astro:content";
+
+// function parseFileName(fileName: string): string {
+//   // parse information from a collection entry's file name
+//   getEntry()
+// }
+
+// export default function patchEntry(entry: CollectionEntry) {
+
+// }
