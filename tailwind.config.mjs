c717c3e2c32205f40f1c6259a518e188398b0b24
pixelchai <ppozogo@gmail.com>
2024-10-06 18:33:39 +0100

    advisory values

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,14 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      screens: {
-        "2xs": "375px",
-        xs: "475px",
-      },
+    screens: {
+      "2xs": "375px",
+      xs: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1920px",
     },
   },
   plugins: [],
