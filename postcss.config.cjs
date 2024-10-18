1ba008273b26ffe809032090573ed8d2fa161aa0
pixelchai <ppozogo@gmail.com>
2024-10-18 16:01:09 +0100

    install more postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
     require("tailwindcss/nesting"),
     require("tailwindcss"),
     require("postcss-combine-media-query"),
+    require("postcss-combine-duplicated-selectors")({
+      removeDuplicatedProperties: true,
+      removeDuplicatedValues: true,
+    }),
     require("autoprefixer"),
+    require("cssnano")({
+      preset: "advanced",
+    }),
+    require("postcss-reporter"),
   ],
 };
