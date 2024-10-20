567d0647063d7d985f360cf76205a314e22ec1d8
pixelchai <ppozogo@gmail.com>
2024-10-20 21:42:28 +0100

    project card styling

--- src/utils/markupUtils.ts
+++ src/utils/markupUtils.ts
@@ -2,3 +2,11 @@ export function combineClasses(...classes: (string | undefined)[]): string {
   // combine string of clases (separated by space) and remove undefined values, remove duplicates
   return Array.from(new Set(classes.filter(Boolean))).join(" ");
 }
+
+export function formatDate(date: Date): string {
+  return date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+}
