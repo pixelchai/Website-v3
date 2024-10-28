784781af391ec9d69ee1ecb9b25874cbd3a3b573
pixelchai <ppozogo@gmail.com>
2024-10-28 19:21:23 +0000

    work on programmatically getting root path

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,5 +1,7 @@
 import { promisify } from "util";
 import { exec } from "child_process";
+import { fileURLToPath } from "node:url";
+import { dirname } from "path";
 
 export async function getExecStdout(cmd: string) {
   try {
@@ -8,4 +10,20 @@ export async function getExecStdout(cmd: string) {
   } catch {
     return "";
   }
+}
+
+export function getProjectRootPath(): string {
+  const curFilename = fileURLToPath(import.meta.url);
+
+  let rootPath = dirname(curFilename);
+  // go upwards until we find package.json
+  while (!rootPath.endsWith("/src")) {
+    rootPath = dirname(rootPath);
+
+    if (rootPath === "/") {
+      throw new Error("Could not find project root path");
+    }
+  }
+
+  return rootPath;
 }
